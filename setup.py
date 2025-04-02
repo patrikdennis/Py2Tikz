7951fe4c51e3ae9e6267c778eb4f3adc1f079084
Patrik Dennis <patrik.franzen.dennis@gmail.com>
2025-04-02 03:24:28 +0200

    refactor: change name

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pytikz",
+    name="Py2Tikz",
     version="0.0.1",
     author="Patrik Franzén",
     author_email="patrik.franzen.dennis@gmail.com",
     description="A Python library for generating LaTeX pgfplots/tikz code.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/patrikdennis/pytikz",
+    url="https://github.com/patrikdennis/Py2Tikz",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
